245b421fbca91698c493baef1022d61767c8437d
emanuel <edoliveira@sga.pucminas.br>
2021-06-25 16:50:48 -0300

    Full tutorial

---
+++ src/NotFound.js
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+const NotFOund = () => {
+    return ( 
+        <div className="not-found">
+            <h2>Sorry</h2>
+            <p>That page cannot be found</p>
+            <Link to="/">Back to thehome page...</Link>
+        </div>
+     );
+}
+ 
+export default NotFOund;
